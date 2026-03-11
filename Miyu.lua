fdcec1dee5b5959ba4489e9a8ef45abc23d49c06
Miyuxg <kazexpro05@gmail.com>
2026-03-11 15:25:16 +0700

    Miyu.lua

--- Miyu.lua
+++ Miyu.lua
@@ -1,6 +1,6 @@
 getgenv().ScriptTitle = "Miyu [ Miyu by Hai1110#]"
 getgenv().ScriptSubTitle = "Miyu - V1.0"
 getgenv().ScriptImage = "https://i.ibb.co/mF1TqJBD/att-1z9em-Owggba-RSHFhu-Ghzm-X47lq-Apxsh8-CQcvtews-HE-png.jpg"
-getgenv().ScriptAuthorName = "Hai1110#"
+getgenv().ScriptAuthorName = "Miyu 🥀"
 getgenv().ScriptAuthorSubTitle = "Discord: *******"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
